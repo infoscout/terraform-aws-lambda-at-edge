4fac527ea85cfd7981c60c4858b843d15c63eec0
shuyanl3 <sli316@outlook.com>
2021-04-26 12:19:45 -0500

    fix lambda layer, change terraform version to >=v0.12.29

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@
  * Creates a Lambda@Edge function to integrate with CloudFront distributions.
  */
 
-resource "aws_lambda_layer_version" "lambda_layer" {
-  count      = var.use_lambda_layer ? 1 : 0
-  filename   = "${var.lambda_layer_path}"
-  layer_name = var.lambda_layer_name
-
-  compatible_runtimes = var.lambda_layer_compatible_runtimes
-}
-
 /**
  * Lambdas are uploaded to via zip files, so we create a zip out of a given directory.
  * In the future, we may want to source our code from an s3 bucket instead of a local zip.
@@ -73,7 +65,7 @@ resource aws_lambda_function lambda {
   runtime = var.runtime
   role    = aws_iam_role.lambda_at_edge.arn
   tags    = var.tags
-  layers  = var.use_lambda_layer ? [aws_lambda_layer_version.lambda_layer.arn] : null
+  layers  = var.lambda_layers
 
   lifecycle {
     ignore_changes = [
